a472267c200baaae524f13b2ee35bce3dcacad97
gimyojun <gywnsdl5623@gmail.com>
2023-10-31 09:19:44 +0900

    기능개발 중...1

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -2,6 +2,9 @@ package com.ll;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        System.out.println("기능개발1");
+        System.out.println("기능개발2");
     }
 }
